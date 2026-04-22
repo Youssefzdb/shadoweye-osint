2302fc7e28c702ab1da05b5e2371d115284fbd4d
Youssefzdb <149632182+Youssefzdb@users.noreply.github.com>
2026-04-22 23:45:45 +0100

    feat: update utils/logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 class Logger:
-    def __init__(self, verbose=False):
-        self.verbose = verbose
-    def info(self, m):    print(f"\033[94m[*]\033[0m {m}")
-    def success(self, m): print(f"\033[92m[+]\033[0m {m}")
-    def warning(self, m): print(f"\033[93m[!]\033[0m {m}")
-    def error(self, m):   print(f"\033[91m[-]\033[0m {m}")
+    R="\033[91m"; G="\033[92m"; Y="\033[93m"; B="\033[94m"; X="\033[0m"
+    def __init__(self, verbose=False): self.verbose = verbose
+    def info(self, m): print(f"{self.B}[*]{self.X} {m}")
+    def success(self, m): print(f"{self.G}[+]{self.X} {m}")
+    def warning(self, m): print(f"{self.Y}[!]{self.X} {m}")
+    def error(self, m): print(f"{self.R}[-]{self.X} {m}")
     def debug(self, m):
-        if self.verbose:  print(f"\033[90m[D]\033[0m {m}")
+        if self.verbose: print(f"[DBG] {m}")
