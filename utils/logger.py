8a50b28f15b261cebbb7a03eaa156d8743c74342
Youssefzdb <149632182+Youssefzdb@users.noreply.github.com>
2026-04-22 23:44:40 +0100

    feat: add file

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
 class Logger:
     def __init__(self, verbose=False):
         self.verbose = verbose
-    def info(self, m): print(f"\033[94m[*]\033[0m {m}")
+    def info(self, m):    print(f"\033[94m[*]\033[0m {m}")
     def success(self, m): print(f"\033[92m[+]\033[0m {m}")
     def warning(self, m): print(f"\033[93m[!]\033[0m {m}")
-    def error(self, m): print(f"\033[91m[-]\033[0m {m}")
+    def error(self, m):   print(f"\033[91m[-]\033[0m {m}")
     def debug(self, m):
-        if self.verbose: print(f"[D] {m}")
+        if self.verbose: print(f"[DEBUG] {m}")
